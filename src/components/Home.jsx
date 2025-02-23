2b058684d56ebfd5b358c644ba3a4336f2899d8b
auntim <hossennull@gmail.com>
2025-02-24 01:14:51 +0600

    add some frame motion in name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,42 @@ import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+const text = "Hi, Myself Auntim Hossen Saikat!";
 
 const Home = () => {
+
+    const textRef = useRef(null);
+    let index = 0;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (textRef.current) {
+                textRef.current.innerText = text.slice(0, index + 1); // Add one letter at a time
+                index = (index + 1) % (text.length + 1); // Reset after full sentence
+            }
+        }, 150);
+
+        return () => clearInterval(interval);
+    }, []);
     return (
         <div
             name="home"
             className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
         >
             <div className="max-w-screen-lg mx-auto  flex flex-col items-center justify-center h-full  px-4 md:flex-row">
-                <div className="flex flex-col justify-center mt-40 md:mt-0 h-full">
-                    <h3 className="text-2xl font-semibold text-[#A0EFFF] ">Hi,Myself Auntim Hossen Saikat!</h3>
+                <div className="flex flex-col justify-center mt-40 md:mt-20 h-full">
+                    <motion.h3
+                        ref={textRef}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                        className="text-2xl font-semibold text-[#A0EFFF] mt-36"
+                    >
+                        {/* Letters will appear dynamically */}
+                    </motion.h3>
                     <h2 className="text-4xl sm:text-7xl font-bold text-white">
                         I'm a Junior MERN Stack Developer
                     </h2>
@@ -41,7 +67,7 @@ const Home = () => {
                     <img
                         src={HeroImage}
                         alt="my profile"
-                        className="rounded-full mx-auto w-3/4  md:h-[400px] md:w-[650px] ml-20 items-center"
+                        className="rounded-full mx-auto w-full  md:h-[400px] md:w-[750px] md:ml-20 items-center border-8 border-fuchsia-700"
                     />
                 </div>
             </div>
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
                     LinkedIn <FaLinkedin size={30} />
                 </>
             ),
-            href: "https://linkedin.com",
+            href: "https://www.linkedin.com/in/auntim-hossen-saikat/",
             style: "rounded-tr-md",
         },
         {
